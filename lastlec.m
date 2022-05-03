f93780371afa848d8d5cab8ea2b8fbca07877f6c
Piperliam <68978670+piperliam@users.noreply.github.com>
2022-05-02 20:06:53 -0400

    Update lastlec.m

--- lastlec.m
+++ lastlec.m
@@ -2,3 +2,4 @@
 my_sys = tf([1], [1 5]); %just an example with g(s)=1/(s+5) where [1] is numberator, 1 is the power for s and 5 is the representive of 5
 bode(my_sys)
 %sidenote, this acts as a low pass filter
+%erm
